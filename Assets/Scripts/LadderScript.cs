955c66b1c6b3c1b25041272204ec039287b6011b
muyangyan1 <muyangyan.me@gmail.com>
2019-05-18 20:47:18 -0700

    first commit

---
+++ Assets/Scripts/LadderScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderScript : MonoBehaviour {
+
+    bool inside = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag=="Ladders")
+        {
+            inside = !inside;
+
+            Debug.Log("boop");
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag=="Ladders")
+        {
+            inside = !inside;
+            Debug.Log("boop");
+        }
+    }
+    void Update ()
+    {
+        if (Input.GetKey("w") && inside==true)
+        {
+            transform.Translate(Vector3.up * 20 * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitch : MonoBehaviour {
+	public void GoToLevel (string level)
+    {
+        Application.LoadLevel(level);
+	}
+}
